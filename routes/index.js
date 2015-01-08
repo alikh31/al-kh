60eeda0e9de6fa60fb3ba628b2a93961d747977d
root <al_kh31@yahoo.com>
2015-01-08 17:36:26 -0500

    minor changes

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,18 @@
  */
 
 var geoip = require('geoip-lite');
+var path = require('path');
 
 exports.index = function (req, res) {
-	var geo = geoip.lookup(req.connection.remoteAddress);
-	console.log(geo);
+    var geo = geoip.lookup(req.connection.remoteAddress);
+    
+    var date = new Date();
 
-    res.sendfile('views/index.html');
+    console.log(date);
+
+    console.log(req.headers);
+    console.log(req.connection.remoteAddress);
+    console.log(geo);
+
+    res.sendfile(path.join(__dirname,'../views/index.html'));
 };
